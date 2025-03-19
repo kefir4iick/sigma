180d90b9eec397fd710a0708cfe4aa8a2a5134ce
kefir4iick <lizardm17@icloud.com>
2025-03-19 13:41:04 +0200

    add use middleware

--- server/program.cs
+++ server/program.cs
@@ -1,5 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 var app = builder.Build();
+app.UseMiddleware<LoggingMiddleware>();
 app.MapControllers();
 app.Run();
