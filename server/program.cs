fec0cdd43c2ac8de23f550c6d5f72f4db470eefe
kefir4iick <lizardm17@icloud.com>
2025-03-19 17:45:10 +0200

    add cors

--- server/program.cs
+++ server/program.cs
@@ -1,6 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy => policy.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+});
+
 var app = builder.Build();
+app.UseCors("AllowAll");
 app.UseMiddleware<LoggingMiddleware>();
 app.MapControllers();
 app.Run();
