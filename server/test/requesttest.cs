8e997627029c6de7e7a1c39ff62effddf2f1f6c3
kefir4iick <lizardm17@icloud.com>
2025-03-29 09:04:35 +0200

    update

--- server/test/requesttest.cs
+++ server/test/requesttest.cs
@@ -5,12 +5,12 @@ public class RequestTest
     [Fact]
     public void Test()
     {
-        var requestService = new RequestService();
+        IRequestService requestService = new RequestService();
 
-        var request_1 = requestService.count();
-        var request_2 = requestService.count();
+        var request1 = requestService.count();
+        var request2 = requestService.count();
         
-        Assert.Equal(1, request_1);
-        Assert.Equal(2, request_2);
+        Assert.Equal(1, request1);
+        Assert.Equal(2, request2);
     }
 }
